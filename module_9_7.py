e5bc36c3de7a34d750bc6e74a2ef9dc865078c19
Павел Сотченков <dr.sot4enkoff2017@yandex.ru>
2024-09-11 16:28:06 +0500

    Add files via upload

---
+++ module_9_7.py
@@ -0,0 +1,22 @@
+def is_prime_dec(func):
+    def wrapper(a, b, c):
+        dec_res = func(a, b, c)
+        for i in range(2, dec_res):
+            if dec_res % i == 0:
+                print('Составное')
+                break
+            else:
+                print('Простое')
+                break
+        return dec_res
+
+    return wrapper
+
+
+@is_prime_dec
+def sum_three(a, b, c):
+    return a + b + c
+
+
+res = sum_three(2, 4, 6)
+print(res)
